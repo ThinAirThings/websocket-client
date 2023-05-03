7642ab0616760362ecadf601152c2d2c2eb87dea
irishcarbomb777 <danielclannan@gmail.com>
2023-05-03 15:03:01 -0400

    Initial commit

---
+++ src/SocketioClient.ts
@@ -0,0 +1,23 @@
+import { io, Socket } from "socket.io-client"
+import { rxToTx } from "./txTx"
+
+export class WebsocketClient{
+    socket: Socket
+    connected: Promise<void>
+    constructor(url: string, actions: Record<string, any>){
+        this.socket = io(url)
+        this.connected = new Promise((resolve) => {
+            this.socket.on('connect', () => {
+                console.log(`Client Connected to: ${url}`)
+                resolve()
+            })
+        })
+        for (const [action, callback] of Object.entries(actions)){
+            this.socket.on(rxToTx(action), callback)
+        }
+    }
+    sendMessage = async (action: string, payload: Record<string, any>) => {
+        await this.connected
+        this.socket.emit(action, payload)
+    }
+}
---
+++ src/websocketFetch.ts
@@ -0,0 +1,42 @@
+import {nanoid} from 'nanoid'
+type IncomingWebsocketRequestMessage = {
+    messageId: string
+    status: 'RUNNING'|'ERROR'|'COMPLETE'
+    payload: Record<string, any>
+}
+
+export const websocketFetch = async <R=Record<string, any>, UpdatePayload extends Record<string, any>=Record<string, any>>(
+    url: string, 
+    action: string, 
+    payload: Record<string, any>, 
+    handleUpdateMessage?: (payload: UpdatePayload)=>void
+): Promise<R> => {
+    const messageId = nanoid()
+    return new Promise<R>((resolve, reject) => {
+        const websocket = new WebSocket(url)
+        websocket.onopen = () => {
+            websocket.onmessage = (event) => {
+                const data = JSON.parse(event.data) as IncomingWebsocketRequestMessage
+                if (data.messageId === messageId) {
+                    if (data.status === 'RUNNING') {
+                        handleUpdateMessage?.(data.payload as UpdatePayload)
+                    } else if (data.status === 'COMPLETE') {
+                        resolve(data.payload as R)
+                        websocket.close()
+                    } else if (data.status === 'ERROR') {
+                        reject(data.payload)
+                        websocket.close()
+                    } else {
+                        reject('Unknown status')
+                        websocket.close()
+                    }
+                }
+            }
+            websocket.send(JSON.stringify({
+                action,
+                messageId,
+                ...payload
+            }))
+        }
+    })
+}
