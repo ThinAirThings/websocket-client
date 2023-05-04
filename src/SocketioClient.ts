827d3ca0b06760dee7813e461f2667d74071da05
irishcarbomb777 <danielclannan@gmail.com>
2023-05-04 07:33:20 -0400

    mod

--- src/SocketioClient.ts
+++ src/SocketioClient.ts
@@ -22,17 +22,17 @@ export class SocketioClient{
         await this.connected
         this.socket.emit(action, payload)
     }
-    fetch = async <R=Record<string, unknown>, UpdatePayload extends Record<string, any>=Record<string, any>>(
+    fetch = async <UpdatePayload extends Record<string, any>=Record<string, any>>(
         action: string, 
-        payload: Record<string, any>, 
+        payload?: Record<string, any>, 
         handleUpdateMessage?: (payload: UpdatePayload)=>void
-    ): Promise<R> => {
+    ): Promise<Record<string, unknown>> => {
         await this.connected
         const messageId = nanoid()
-        return new Promise<R>((resolve, reject) => {
+        return new Promise<Record<string, unknown>>((resolve, reject) => {
             this.socket.on(messageId, (data: IncomingWebsocketRequestMessage) => {
                 if (!data.status || data.status === 'COMPLETE') {
-                    resolve(data.payload as R)
+                    resolve(data.payload)
                 } else if (data.status === 'RUNNING') {
                     handleUpdateMessage?.(data.payload as UpdatePayload)
                 } else if (data.status === 'ERROR') {
